fd21fe1b3485b6c0ccdf5176514e70ca4d725e6d
Jiweon Park <rkawk3142@gmail.com>
2019-11-08 13:17:24 -0800

    moved files into routes directory - updated crud function by hy

--- travels.js
+++
@@ -1,8 +0,0 @@
-//import express from 'express';
-//import mysqlConnection from '../connection';
-const express = require("express");
-const router = express.Router();
-const bodyParser = require('body-parser');
-const mysqlConnection = require('../connection.js');
-
-module.exports = router;
