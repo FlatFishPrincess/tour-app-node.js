60d2124dadf569d17b83c9458fd3c4b3b631e01d
Jiweon Park <rkawk3142@gmail.com>
2020-03-02 17:52:04 -0800

    fixed deploy bug issues

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ app.use('/get', getRouter);
 app.use('/update', updateRouter);
 app.use('/delete', deleteRouter);
 
-// localhost:3000
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
   console.log('listening 3000');
 });
