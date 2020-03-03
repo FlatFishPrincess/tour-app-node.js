55d9e740b0435a858ee9c02642a47a8e290380c7
Jiweon Park <rkawk3142@gmail.com>
2020-03-02 17:27:59 -0800

    deployed database && changed database connection

--- connection.js
+++ connection.js
@@ -5,12 +5,12 @@ const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
   // socketPath needs for MAMP, if u do not use it, just comment out
-  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-  host: 'localhost',
-  user: 'root',
+  // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+  host: 'us-cdbr-iron-east-04.cleardb.net',
+  user: 'b10678b3bdcb6b',
   // make sure ur password is correct
-  password: 'root',
-  database: 'tourreview',
+  password: '16773c45',
+  database: 'heroku_f329b333647dd97',
   multipleStatements: true
 });
 
